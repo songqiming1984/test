79817dbad160576d02d2a600596c5089f116f242
song <songqiming1984@qq.com>
2020-04-27 09:39:19 +0800

    an.py怎加打印内容

--- an.py
+++ an.py
@@ -6,5 +6,6 @@ import sys
 driver = webdriver.Chrome()
 driver.get('http://www.baidu.com')
 time.sleep(3)
-print('执行脚本路径：'+sys.argv[0])
+print('执行脚本绝对路径：'+sys.argv[0])
+print('执行脚本相对路径：'+__file__)
 driver.close()
